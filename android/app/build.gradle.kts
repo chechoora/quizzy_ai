d856cd0dcd33eb8ed717c3b2fc8010941d0e2ca1
kyrylokharchenko <kharchenko.kir@gmail.com>
2026-01-18 15:00:23 +0200

    [] Bump Android app version code
    - Incremented version code from 3 to 4
    - Prepared for new app release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
         applicationId = "com.chechoora.quizzy"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 3
+        versionCode = 4
         versionName = flutter.versionName
     }
 
