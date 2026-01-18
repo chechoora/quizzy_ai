661411358ffe7a2ec124bf0cfed26db24afcae9b
kyrylokharchenko <kharchenko.kir@gmail.com>
2026-01-18 17:16:24 +0200

    [] Bump version code and name
    - Incremented Android version code from 6 to 7
    - Updated pubspec version from 1.0.2+1 to 1.0.3+1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
         applicationId = "com.chechoora.quizzy"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 6
+        versionCode = 7
         versionName = flutter.versionName
     }
 
