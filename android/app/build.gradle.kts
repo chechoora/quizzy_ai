ed7271c73e06cda918331d0357bb677d49928fe7
kyrylokharchenko <kharchenko.kir@gmail.com>
2026-07-19 21:56:47 +0300

    [] Configure iCloud integration
    - Added iCloud container identifiers to iOS entitlements for CloudKit support
    - Updated Android version code from 26 to 27 for app release
    - Reorganized IDE bookmarks for better project navigation

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
         applicationId = "com.chechoora.quizzy"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 26
+        versionCode = 27
         versionName = flutter.versionName
     }
 
